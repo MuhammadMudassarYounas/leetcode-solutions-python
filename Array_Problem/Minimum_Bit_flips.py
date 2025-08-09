6bcf458b521d376458be428834794c8b32757624
MuhammadMudassarYounas <151513066+MuhammadMudassarYounas@users.noreply.github.com>
2025-08-09 19:56:03 +0500

    Problem No 2220

---
+++ Array_Problem/Minimum_Bit_flips.py
@@ -0,0 +1,8 @@
+def MinimumBitFlips(start,goal):
+    ans=start^ goal
+    count=0
+    for i in range(0,32):
+        if (ans &(1<<i)!=0):
+            count+=1
+    return count
+print(MinimumBitFlips(10,7))
